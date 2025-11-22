5f03b10cbf648a5e4c4ccd037ff59652668ffed0
Hyung-Kyu Choi <hyungkyu.choi@gmail.com>
2025-11-22 15:50:14 -0300

    Update MockUSDC with more interfaces
    Update MockUSDC with more interfaces
    
    Signed-off-by: Hyung-Kyu Choi <hyungkyu.choi@gmail.com>

--- contracts/BuenosTickets/contracts/MockUSDC.sol
+++ contracts/BuenosTickets/contracts/MockUSDC.sol
@@ -3,9 +3,15 @@ pragma solidity ^0.8.0;
 
 // Interface for ERC-20 (USDC)
 interface IERC20 {
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function transfer(address recipient, uint256 amount) external returns (bool);
+    function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 // MockUSDC ERC-20 Contract (For Testing/Demonstration)
@@ -16,33 +22,59 @@ contract MockUSDC is IERC20 {
     string public symbol = "MOCKUSDC";
     uint8 public decimals = 6; // Standard for USDC
 
-    mapping(address => uint256) public balances;
-    mapping(address => mapping(address => uint256)) public allowance;
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
 
     constructor() {
         // Mint a starting supply to the deployer (which will be the Admin/Test account)
-        uint256 initialSupply = 1000000 * 10**decimals;
-        balances[msg.sender] = initialSupply;
+        _totalSupply = 1000000 * 10**decimals;
+        _balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
-    function balanceOf(address account) public view override returns (uint256) { return balances[account]; }
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
     function transfer(address recipient, uint256 amount) public override returns (bool) {
-        require(balances[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
-        balances[msg.sender] -= amount;
-        balances[recipient] += amount;
-        return true;
-    }
-    function approve(address spender, uint256 amount) public returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        return true;
-    }
-    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
-        require(balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
-        require(allowance[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
+        require(_balances[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
 
-        balances[sender] -= amount;
-        allowance[sender][msg.sender] -= amount;
-        balances[recipient] += amount;
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+        
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[msg.sender][spender] = amount;
+        
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(_balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
+        require(_allowances[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[sender] -= amount;
+        _allowances[sender][msg.sender] -= amount;
+        _balances[recipient] += amount;
+        
+        emit Transfer(sender, recipient, amount);
         return true;
     }
 }
